256654f94cbceea01df66ff8537a262f3b90cc92
jip verhoeven <jipverhoeven25@outlook.com>
2025-02-13 11:51:32 +0100

    added a userlist and the function to add users

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './HeaderComponent';
+import UserList from './UserList';
+import AddUser from './AddUser';
 
 function App() {
   return (
     <div>
       <HeaderComponent />
+      <UserList />
+      <AddUser />
     </div>
   );
 }
