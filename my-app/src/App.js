4c28540716fe3dbcd06399b1e1f0d0216d04604a
jip verhoeven <jipverhoeven25@outlook.com>
2025-02-26 08:22:40 +0100

    ff voor die backup gekkies😜

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './HeaderComponent';
-import UserList from './UserList';
-import AddUser from './AddUser';
+import UserList from './app/components/AddUser/UserList/UserList';
+import AddUser from './app/components/AddUser/AddUser';
+
 
 function App() {
   return (
     <div>
       <HeaderComponent />
       <UserList />
-      <AddUser />
     </div>
   );
 }
---
+++ my-app/src/pages/AddUsers/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddUser from '../../app/components/AddUser/AddUser';
+
+function AddUsers() {
+    return (
+    <AddUser />
+    );
+}
+
+export default AddUsers;
